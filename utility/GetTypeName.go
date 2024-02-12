31b4e52ce5504232a68e67db59dc1e20ce7cd67f
heaven-chp <62167253+heaven-chp@users.noreply.github.com>
2024-02-12 14:30:52 +0900

    [#80] [Doc comments] edit

---
+++ utility/GetTypeName.go
@@ -0,0 +1,11 @@
+// Package utility provides utility implementations.
+package utility
+
+import "reflect"
+
+// GetTypeName is get the type name.
+//
+// ex) typeName := utility.GetTypeName(int(1))
+func GetTypeName(value interface{}) string {
+	return reflect.TypeOf(value).String()
+}
