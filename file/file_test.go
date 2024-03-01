15babe8b2ce9c37917d3fb53cd9dfe5dbb1951fa
heaven-chp <62167253+heaven-chp@users.noreply.github.com>
2024-03-01 14:36:06 +0900

    [#86] [file] List develop

--- file/file_test.go
+++ file/file_test.go
@@ -2,8 +2,10 @@ package file_test
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/heaven-chp/common-library-go/file"
 )
 
@@ -54,4 +56,63 @@ func TestWrite(t *testing.T) {
 	}
 
 	os.Remove(fileName)
+}
+
+func TestList(t *testing.T) {
+	dir01 := uuid.New().String() + string(filepath.Separator)
+	defer os.RemoveAll(dir01)
+
+	dir02 := dir01 + uuid.New().String() + string(filepath.Separator)
+	if err := os.MkdirAll(dir02, os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+
+	dir03 := dir01 + uuid.New().String() + string(filepath.Separator)
+	if err := os.MkdirAll(dir03, os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+
+	file01 := dir01 + uuid.New().String() + ".txt"
+	flag01 := int(os.O_WRONLY | os.O_APPEND | os.O_CREATE)
+	if err := file.Write(file01, []string{}, flag01, 0600); err != nil {
+		t.Fatal(err)
+	}
+
+	file02 := dir03 + uuid.New().String() + ".txt"
+	flag02 := int(os.O_WRONLY | os.O_APPEND | os.O_CREATE)
+	if err := file.Write(file02, []string{}, flag02, 0600); err != nil {
+		t.Fatal(err)
+	}
+
+	answer := map[string]bool{}
+
+	answer = map[string]bool{dir02: false, dir03: false, file01: false}
+	if list, err := file.List(dir01, false); err != nil {
+		t.Fatal(err)
+	} else {
+		if len(list) != len(answer) {
+			t.Fatal("invalid list -", list)
+		}
+
+		for _, name := range list {
+			if _, exist := answer[name]; exist == false {
+				t.Fatal("invalid name :", name, list)
+			}
+		}
+	}
+
+	answer = map[string]bool{dir02: true, file01: true, file02: true}
+	if list, err := file.List(dir01, true); err != nil {
+		t.Fatal(err)
+	} else {
+		if len(list) != len(answer) {
+			t.Fatal("invalid list -", list)
+		}
+
+		for _, name := range list {
+			if _, exist := answer[name]; exist == false {
+				t.Fatal("invalid name :", name, list)
+			}
+		}
+	}
 }
