28377e3e62866b97c7c9a33a36139650f204dc26
heaven-chp <62167253+heaven-chp@users.noreply.github.com>
2024-03-01 16:50:33 +0900

    [#88] [lock] mutex, mutex by key develop

---
+++ lock/lock_test.go
@@ -0,0 +1,81 @@
+package lock_test
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/heaven-chp/common-library-go/lock"
+)
+
+func TestMutex(t *testing.T) {
+	mutex := lock.Mutex{}
+
+	mutex.Lock()
+	mutex.Unlock()
+
+	if mutex.TryLock() == false {
+		t.Fatal("invalid")
+	}
+	if mutex.TryLock() {
+		t.Fatal("invalid")
+	}
+	mutex.Unlock()
+
+	const count = 1000
+	result := 0
+	wg := new(sync.WaitGroup)
+	for i := 1; i <= count; i++ {
+		wg.Add(1)
+		go func(index int) {
+			defer wg.Done()
+			mutex.Lock()
+			defer mutex.Unlock()
+
+			result += index
+		}(i)
+	}
+	wg.Wait()
+	if result != count*(count+1)/2 {
+		t.Fatal("invalid - ", count, result)
+	}
+}
+
+func TestMutexByKey(t *testing.T) {
+	mutexs := lock.MutexByKey{}
+
+	test := func(key any) {
+		mutexs.Lock(key)
+		mutexs.Unlock(key)
+
+		mutexs.Delete(key)
+
+		if mutexs.TryLock(key) == false {
+			t.Fatal("invalid")
+		}
+		if mutexs.TryLock(key) {
+			t.Fatal("invalid")
+		}
+		mutexs.UnlockAndDelete(key)
+
+		const count = 1000
+		result := 0
+		wg := new(sync.WaitGroup)
+		for i := 1; i <= count; i++ {
+			wg.Add(1)
+			go func(index int) {
+				defer wg.Done()
+				mutexs.Lock(key)
+				defer mutexs.Unlock(key)
+
+				result += index
+			}(i)
+		}
+		wg.Wait()
+		if result != count*(count+1)/2 {
+			t.Fatal("invalid - ", count, result)
+		}
+	}
+
+	test(1)
+	test("a")
+}
