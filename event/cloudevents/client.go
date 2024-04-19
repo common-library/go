e9d7cb9177f23487b6d53b8386db503fa32c791a
heaven-chp <62167253+heaven-chp@users.noreply.github.com>
2024-04-19 22:04:55 +0900

    [#131] [event][cloudevents] development

--- event/cloudevents/client.go
+++ event/cloudevents/client.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 
 	cloudeventssdk "github.com/cloudevents/sdk-go/v2"
-	"github.com/cloudevents/sdk-go/v2/client"
+	cloudeventssdk_client "github.com/cloudevents/sdk-go/v2/client"
 	"github.com/cloudevents/sdk-go/v2/protocol/http"
 )
 
@@ -19,22 +19,22 @@ const (
 // NewHttp creates and returns a http client.
 //
 // ex) client, err := cloudevents.NewHttp(address, nil, nil)
-func NewHttp(address string, httpOption []http.Option, clientOption []client.Option) (*Client, error) {
+func NewHttp(address string, httpOption []http.Option, clientOption []cloudeventssdk_client.Option) (*client, error) {
 	if protocol, err := cloudeventssdk.NewHTTP(httpOption...); err != nil {
 		return nil, err
-	} else if client, err := cloudeventssdk.NewClient(protocol, clientOption...); err != nil {
+	} else if clientOfSdk, err := cloudeventssdk.NewClient(protocol, clientOption...); err != nil {
 		return nil, err
 	} else {
-		return &Client{clientType: clientTypeHttp, client: client, address: address}, nil
+		return &client{clientType: clientTypeHttp, clientOfSdk: clientOfSdk, address: address}, nil
 	}
 }
 
-// Client is a struct that provides client related methods.
-type Client struct {
+// client is a struct that provides client related methods.
+type client struct {
 	clientType clientType
 
-	client  client.Client
-	address string
+	clientOfSdk cloudeventssdk_client.Client
+	address     string
 
 	wgForReceiver         sync.WaitGroup
 	cancelFuncForReceiver context.CancelFunc
@@ -43,15 +43,15 @@ type Client struct {
 // Send transmits an event.
 //
 // ex) result := client.Send(event)
-func (this *Client) Send(event Event) Result {
-	return Result{result: this.client.Send(this.getContext(), event)}
+func (this *client) Send(event Event) Result {
+	return Result{result: this.clientOfSdk.Send(this.getContext(), event)}
 }
 
 // Send transmits an event and returns a response event.
 //
 // ex) responseEvent, result := client.Request(event)
-func (this *Client) Request(event Event) (*Event, Result) {
-	responseEvent, result := this.client.Request(this.getContext(), event)
+func (this *client) Request(event Event) (*Event, Result) {
+	responseEvent, result := this.clientOfSdk.Request(this.getContext(), event)
 
 	return responseEvent, Result{result: result}
 }
@@ -63,7 +63,7 @@ func (this *Client) Request(event Event) (*Event, Result) {
 //	httpOption := []cloudeventssdk_http.Option{cloudeventssdk_http.WithPort(port)}
 //	receiveclient, err := cloudevents.NewHttp("", httpOption, nil)
 //	receiveclient.StartReceiver(handler, failureFunc)
-func (this *Client) StartReceiver(handler func(context.Context, Event), failureFunc func(error)) {
+func (this *client) StartReceiver(handler func(context.Context, Event), failureFunc func(error)) {
 	this.wgForReceiver.Add(1)
 	go func() {
 		defer this.wgForReceiver.Done()
@@ -71,7 +71,7 @@ func (this *Client) StartReceiver(handler func(context.Context, Event), failureF
 		ctx, cancel := context.WithCancel(this.getContext())
 		this.cancelFuncForReceiver = cancel
 
-		if err := this.client.StartReceiver(ctx, handler); err != nil {
+		if err := this.clientOfSdk.StartReceiver(ctx, handler); err != nil {
 			failureFunc(err)
 		}
 	}()
@@ -80,14 +80,14 @@ func (this *Client) StartReceiver(handler func(context.Context, Event), failureF
 // StopReceiver stops receiving events by StartReceiver.
 //
 // ex)client.StopReceiver()
-func (this *Client) StopReceiver() {
+func (this *client) StopReceiver() {
 	if this.cancelFuncForReceiver != nil {
 		this.cancelFuncForReceiver()
 	}
 	this.wgForReceiver.Wait()
 }
 
-func (this *Client) getContext() context.Context {
+func (this *client) getContext() context.Context {
 	switch this.clientType {
 	case clientTypeHttp:
 		return cloudeventssdk.ContextWithTarget(context.Background(), this.address)
